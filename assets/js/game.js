fe5826d25306889406585c84cb452105fd920b3f
briancollins02 <brianmcollins0@gmail.com>
2022-09-27 21:32:37 -0400

    integrate high scores and write questions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,41 +1,85 @@
 const questionEl = document.getElementById('question-render');
 const timerEl = document.getElementById('ui-time-remaining');
 const pointsEl = document.getElementById('ui-question-progress');
-const buttons = document.querySelectorAll('.answer-choice');
 const quizLengthEl = document.getElementById('total-questions');
-const saveScoreEl = document.getElementById('submit-button');
+const highScoreEl = document.getElementById('high-scoreEl');
 var initialInput = document.getElementById('initialsEl');
+
+const endScore = document.getElementById('total-scoreEl');
+const totalQuestions = document.getElementById('total-questionsEl');
 
 const choiceA = document.getElementById('a');
 const choiceB = document.getElementById('b');
 const choiceC = document.getElementById('c');
 const choiceD = document.getElementById('d');
 
-var timeLeft = 4;
+var canAnswer = false;
+
+var savedHighScore = JSON.parse(localStorage.getItem('high-score')) || 0;
+var scoreDisplay = savedHighScore.pointTotal
+
+console.log(savedHighScore)
+console.log(scoreDisplay)
+
+console.log('old high-score',scoreDisplay)
+
+
+var timeLeft = 29;
 var score = 0;
 var currentQuestion;
 const questions = [
     {
-        q: 'the answer to this question is "hi"',
-        choices: ['a. howdy', 'b. hey', 'c. hi', 'd.hehe'],
+        q: 'When was "The Titanic" released??',
+        choices: ['a. 1990', 'b. 2000', 'c. 1997', 'd. 1995'],
         answer: 'c'
     },
     {
-        q: 'the answer to this question is "no"',
-        choices: ['a. nawr', 'b. nope', 'c. nada', 'd.no'],
+        q: 'What is the highest grossing movie of all time?',
+        choices: ['a. Avengers: Endgame', 'b. Star Wars: Episode VII', 'c. The Lion King', 'd. Avatar'],
         answer: 'd'
     },
     {
-        q: 'the answer to this question is "growl"',
-        choices: ['a. growl', 'b. bark', 'c. ribbit', 'd.meow'],
+        q: 'how many films hold 11 Academy Award winds?',
+        choices: ['a. 3', 'b. 1', 'c. 5', 'd. 7'],
         answer: 'a'
     },
     {
-        q: 'the answer to this question is "thunk"',
-        choices: ['a. think', 'b. thunk', 'c. thank', 'd.thonk'],
+        q: 'What was the first "fantasy" movie to win best picture',
+        choices: ['a. The Return of the King', 'b. Harry Potter', 'c. Fellowship of the Ring', 'd. Willy Wonka'],
+        answer: 'a'
+    },
+    {
+        q: 'Which one of these movies holds a 100 on metacritic?',
+        choices: ["a. Pan's Labyrinth", 'b. Moonlight', 'c. Psycho', 'd. The GodFather'],
+        answer: 'd'
+    },
+    {
+        q: 'What year did the first Academy Awards take place?',
+        choices: ['a. 1929', 'b. 1970', 'c. 1956', 'd. 1922'],
+        answer: 'a'
+    },
+    {
+        q: 'What actor is widely blamed for the precense of celebrity voice actors in animated movies?',
+        choices: ['a. Danny Devito', 'b. Robin Williams', 'c. Billy Idol', 'd. Tom Hanks'],
         answer: 'b'
+    },
+    {
+        q: 'What was the first Feature Film John Williams composed for?',
+        choices: ['a. Star Wars', 'b. Daddy-O', 'c. Jaws', 'd. The Killers'],
+        answer: 'b'
+    },
+    {
+        q: 'This low-budget horror director was given free reign to Lord of the Rings',
+        choices: ['a. Stephen Spielberg', 'b. Geroge Lucas', 'c. Peter Jackson', 'd. Guillermo del Torro'],
+        answer: 'c'
+    },
+    {
+        q: 'When was the first Star Wars movie released?',
+        choices: ['a. 1980', 'b. 1983', 'c. 1976', 'd. 1977'],
+        answer: 'd'
     }
 ]
+// Total numer of Questions
 quizLengthEl.innerText = '/' + questions.length
 
 // Define Functions
@@ -49,16 +93,20 @@ function countdown() {
         timerEl.textContent = timeLeft + ' second remaining';
         timeLeft--;
       } else {
-        timerEl.textContent = '';
+        timerEl.textContent = ' 0 seconds remaining';
         clearInterval(timeInterval);
         endGame()
       }
     }, 1000);
 }
 
+// start out at -1 so get question gets us to index: 0
 var arraySelection = -1
 
+// Go to next question
 function getQuestion() {
+    // enable answering
+    canAnswer = true;
     //check if there are any more questions
  
     if (arraySelection > questions.length-2) {
@@ -72,36 +120,36 @@ function getQuestion() {
     choiceB.textContent = currentQuestion.choices[1];
     choiceC.textContent = currentQuestion.choices[2];
     choiceD.textContent = currentQuestion.choices[3];
-    console.log(arraySelection);
 }
 
 // check button selection and add score
 document.querySelectorAll('button').forEach(occurence => {
-    occurence.addEventListener('click', (e) => {
-        let elementId = e.target.id;
-        if (elementId !== '') {
-            console.log(elementId, ' was clicked');
-            console.log(questions[arraySelection].answer, 'is the correct answer')
-            if (elementId == questions[arraySelection].answer){
-                console.log('correct!');
-                score++
-                pointsEl.textContent = score + ' correct/' + questions.length;
-                correct();
-            } else {
-                console.log('wrong!');
-                timeLeft -=5;
-                incorrect()
+        occurence.addEventListener('click', (e) => {
+            if (canAnswer == true) {
+                let elementId = e.target.id;
+                console.log(canAnswer)
+                if (elementId !== '') {
+                    if (elementId == questions[arraySelection].answer){
+                        score++
+                        pointsEl.textContent = score + ' correct/' + questions.length;
+                        correct();
+                    } else {
+                        timeLeft -=5;
+                        incorrect()
+                    }
+                }
+            } else { //stop the function from working while canAnswer=false
+                return
             }
-        }
-        //TODO insert a function that plays a confirmation animation
-        //wait for user feedback to play and then move to the next question
-        setTimeout(getQuestion, 1000); 
+            //TODO insert a function that plays a confirmation animation
+            //wait for user feedback to play and then move to the next question
+        setTimeout(getQuestion, 250); 
     })
 })
 
 //correct and incorrect user feedback
 function correct() {
-    console.log('correct!');
+        canAnswer = false;
         pointsEl.classList.add('flash-green');
     setTimeout(function() {
         pointsEl.classList.remove('flash-green');
@@ -109,7 +157,7 @@ function correct() {
 }
 
 function incorrect() {
-    console.log('incorrect!');
+        canAnswer = false;
         timerEl.classList.add('flash-red');
     setTimeout(function() {
         timerEl.classList.remove('flash-red');
@@ -120,31 +168,45 @@ function incorrect() {
 
 // end game if time up or out of questions
 function endGame() {
-    document.getElementById('end-screen').classList.add('show');
-    document.getElementById('end-screen').classList.remove('hide');
     endScore.textContent = score;
     totalQuestions.textContent = questions.length;
+    console.log('scoreDisplay',scoreDisplay);
+    document.getElementById('end-screen').classList.add('show');
+    document.getElementById('end-screen').classList.remove('hide');
+    console.log('current score', score)
+    if (scoreDisplay === undefined || score > savedHighScore.pointTotal) {
+        console.log(savedHighScore.pointTotal)
+        console.log('new high-score!')
+        //Show High Score submission form
+        document.getElementById('form').classList.add('show')
+        document.getElementById('form').classList.remove('hide')
+    } else {
+        highScoreEl.textContent = 'The current High-Score is ' + scoreDisplay + ' by ' + savedHighScore.initials;
+        document.getElementById('back-button').classList.add('show')
+        document.getElementById('back-button').classList.remove('hide')
+    }
+    
+
+   
+
 }
 
 // End Screen Functionality
-const endScore = document.getElementById('total-scoreEl');
-const totalQuestions = document.getElementById('total-questionsEl') ;
 
 function saveScore() {
-    console.log('submitted!')
     // create user object from submission
     var currentScore = {
         pointTotal: score,
         initials: initialInput.value.trim()
     };
-
+    console.log(savedHighScore)
     //send submission to local storage
-    localStorage.setItem("high-scores", JSON.stringify(currentScore));
+        localStorage.setItem("high-score", JSON.stringify(currentScore));
+        savedHighScore = localStorage.getItem("high-score");
+        console.log('submitted!', savedHighScore)
+    return window.location.assign('index.html')
 }
 
 // Call Functions
 getQuestion();
 countdown();
-
-
-console.log(questions.length)
